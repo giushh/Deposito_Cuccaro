7f607179d771d2772b69e8ecdbc67a95fc7ae08b
Ilaria Cuccaro <89212726+giushh@users.noreply.github.com>
2026-02-05 18:07:11 +0100

    Update Esercizi_funzioni_2.py

--- Corso_Python/05_02_2026/pomeriggio/Esercizi_funzioni_2.py
+++ Corso_Python/05_02_2026/pomeriggio/Esercizi_funzioni_2.py
@@ -9,4 +9,29 @@ programma dovrebbe stampare la sequenza di Fibonacci fino a N.
 Ad esempio, se l'utente inserisce 100, il programma dovrebbe
 stampare tutti i numeri della sequenza di Fibonacci minori o
 uguali a 100.
+
+ps. la sequenza di Fibonacci è una sequenza di numeri in cui i primi due numeri sono 0 e 1
+e ogni numero successivo è la somma dei due precedenti
+(esempio: 0, 1, 1, 2, 3, 5, 8, 13, ...)
 """
+
+def fibonacci(n):
+    """
+    Stampa la sequenza di Fibonacci fino al numero inferiore a n"""
+    
+    a = 0                 # Inizializziamo i primi due numeri della sequenza
+    b = 1
+
+    while a <= n:
+        print(a)      # Stampa il numero corrente della sequenza
+        c = a + b     # Calcola il numero successivo
+        a = b         # Aggiorna il primo valore
+        b = c         # Aggiorna il secondo valore
+
+print("\n********** Sequenza di Fibonacci **********")
+
+n = int(input("Quanti numeri della sequenza di Fibonacci vuoi stampare? "))
+
+fibonacci(n)
+
+print("Fatto, addio")
