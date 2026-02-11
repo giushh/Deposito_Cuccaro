fb3f937ded4a7f7ea13e00222287d0ceeba40332
Ilaria Cuccaro <89212726+giushh@users.noreply.github.com>
2026-02-11 15:33:34 +0100

    update esercizio squadre

--- Corso_Python/11_02_2026/mattina/Esercizio_Squadre/main.py
+++ Corso_Python/11_02_2026/mattina/Esercizio_Squadre/main.py
@@ -64,28 +64,52 @@ print("=== MASTERCHEF: Gara a squadre ===\n")
 squadra1 = Squadra("Squadra Rossa")
 squadra2 = Squadra("Squadra Blu")
 
-# --- Squadra 1 ---
+#********** SQUADRA 1
 squadra1.aggiungi_membro(Allenatore("Chef Marco", 45, 15))
 squadra1.aggiungi_membro(Assistente("Giulia", 29, "Dispensa/Logistica"))
+
+# Antipasti (2)
 squadra1.aggiungi_membro(CuocoAntipasti("Luca", 26, crea_esperienza(5, 2, 2, 1)))
+squadra1.aggiungi_membro(CuocoAntipasti("Irene", 31, crea_esperienza(4, 3, 2, 2)))
+
+# Primi (2)
 squadra1.aggiungi_membro(CuocoPrimi("Sara", 30, crea_esperienza(2, 5, 3, 2)))
+squadra1.aggiungi_membro(CuocoPrimi("Tommaso", 27, crea_esperienza(3, 4, 3, 1)))
+
+# Secondi (2)
 squadra1.aggiungi_membro(CuocoSecondi("Paolo", 33, crea_esperienza(2, 3, 5, 1)))
+squadra1.aggiungi_membro(CuocoSecondi("Valeria", 36, crea_esperienza(2, 2, 4, 2)))
+
+# Dolci (2)
 squadra1.aggiungi_membro(Pasticcere("Anna", 24, crea_esperienza(1, 2, 2, 5)))
-# AGGIUNGI ALTRI MEMBRI PER AVERE PIU SCELTA
-# --- Squadra 2  ---
+squadra1.aggiungi_membro(Pasticcere("Matteo", 29, crea_esperienza(2, 2, 1, 4)))
+
+#********** SQUADRA 2
 squadra2.aggiungi_membro(Allenatore("Chef Elena", 41, 12))
 squadra2.aggiungi_membro(Assistente("Davide", 31, "Impiattamento"))
+
+# Antipasti (2)
 squadra2.aggiungi_membro(CuocoAntipasti("Marta", 27, crea_esperienza(4, 2, 2, 2)))
+squadra2.aggiungi_membro(CuocoAntipasti("Giorgio", 34, crea_esperienza(3, 3, 2, 2)))
+
+# Primi (2)
 squadra2.aggiungi_membro(CuocoPrimi("Nico", 28, crea_esperienza(2, 4, 3, 2)))
+squadra2.aggiungi_membro(CuocoPrimi("Federica", 32, crea_esperienza(3, 5, 2, 2)))
+
+# Secondi (2)
 squadra2.aggiungi_membro(CuocoSecondi("Francesco", 35, crea_esperienza(2, 3, 4, 1)))
+squadra2.aggiungi_membro(CuocoSecondi("Alessia", 26, crea_esperienza(2, 2, 5, 1)))
+
+# Dolci (2)
 squadra2.aggiungi_membro(Pasticcere("Chiara", 23, crea_esperienza(1, 2, 2, 4)))
+squadra2.aggiungi_membro(Pasticcere("Simone", 30, crea_esperienza(2, 2, 2, 5)))
 
 stop = False
 while not stop:
     print("\n--- MENU ---")
     print("1) Mostra squadre")
     print("2) Sfida (selezione portate)")
-    print("0) Esci")
+    print("3) Esci")
     scelta = input("Scelta: ").strip()
 
     match scelta:
@@ -96,11 +120,52 @@ while not stop:
         case "2":
             print("\n== Selezione giocatori per portata ==")
 
+            punti_sq1 = 0
+            punti_sq2 = 0
+            bonus1 = squadra1.bonus_staff_round()
+            bonus2 = squadra2.bonus_staff_round()
+            punti_sq1 += bonus1
+            punti_sq2 += bonus2
+            
+            print(f"Bonus staff -> {squadra1.nome}: +{bonus1} | {squadra2.nome}: +{bonus2}")
+
+
             categorie = [CategoriaAntipasti, CategoriaPrimi, CategoriaSecondi, CategoriaDolci]
             for cat in categorie:
                 print(f"\nPortata: {cat.label().upper()}")
-                _g1 = squadra1.seleziona_giocatore_per_categoria(cat)
-                _g2 = squadra2.seleziona_giocatore_per_categoria(cat)
+                g1 = squadra1.seleziona_giocatore_per_categoria(cat)
+                g2 = squadra2.seleziona_giocatore_per_categoria(cat)
+
+                # se manca qualcuno, salta la portata
+                if g1 is None or g2 is None:
+                    print("Impossibile giocare questa portata (manca un giocatore).")
+                    continue
+
+                p1 = g1.prepara_portata()
+                p2 = g2.prepara_portata()
+
+                if p1 > p2:
+                    punti_sq1 += 3
+                    print(f"Vince la portata: {squadra1.nome} (+3 punti)")
+                elif p2 > p1:
+                    punti_sq2 += 3
+                    print(f"Vince la portata: {squadra2.nome} (+3 punti)")
+                else:
+                    punti_sq1 += 1
+                    punti_sq2 += 1
+                    print("Parità (+1 per ogni squadra)")
+
+            print("\n== RISULTATO FINALE ==")
+            print(f"{squadra1.nome}: {punti_sq1} punti")
+            print(f"{squadra2.nome}: {punti_sq2} punti")
+
+            if punti_sq1 > punti_sq2:
+                print(f"Vince la gara: {squadra1.nome}")
+            elif punti_sq2 > punti_sq1:
+                print(f"Vince la gara: {squadra2.nome}")
+            else:
+                print("La gara finisce in parità")
+
         
         case "3":
             print("Uscita.")
--- Corso_Python/11_02_2026/mattina/Esercizio_Squadre/membri.py
+++ Corso_Python/11_02_2026/mattina/Esercizio_Squadre/membri.py
@@ -36,7 +36,7 @@ class MembroSquadra:
 
     def descrivi(self) -> None:
         print(f"{self.nome} ({self.eta} anni) - membro della squadra.")
-        pass
+        
 
 
 class Giocatore(MembroSquadra):
@@ -55,19 +55,44 @@ class Giocatore(MembroSquadra):
         self.esperienza = esperienza
 
     def mostra_esperienza(self) -> None:
-        pass
+        print(f"\nEsperienza di {self.nome}:")
+
+        categorie = ["antipasti", "primi", "secondi", "dolci"]
+
+        for cat in categorie:
+            valore = self.esperienza[cat]
+            pieno = "■" * valore
+            vuoto = "□" * (5 - valore)
+            print(f" - {cat}: {pieno}{vuoto} ({valore}/5)")
+
 
     def gioca_partita(self) -> int:
-        pass
+        # nelle classi figlie sarà specializzato in prepara_portata()
+        return self.prepara_portata()
+    
+    def prepara_portata(self) -> int:
+        
+    # membrosquadra (nome,eta) -> giocatore (+esperienza) -> ruolo (+categoria)
+
+        # le classi categoria hanno un metodo label() che restituisce il nome della categoria (in questo caso)
+        # e viene usato come chiave per prendere il valore corrispondente nel dizionario
+        chiave = self.categoria.label()
+        livello = self.esperienza[chiave]
+
+        base = random.randint(1, 8)
+        bonus = livello * 3
+        punteggio = base + bonus
+
+        print(f"{self.nome} prepara {chiave}: base={base}, bonus={bonus}, totale={punteggio}")
+
+        return punteggio
 
 
 class CuocoAntipasti(Giocatore):
+        
     def __init__(self, nome: str, eta: int, esperienza: dict):
         super().__init__(nome, eta, esperienza)
-        self.categoria = CategoriaAntipasti
-
-    def prepara_portata(self) -> int:
-        pass
+        self.categoria = CategoriaAntipasti    
 
 
 class CuocoPrimi(Giocatore):
@@ -75,26 +100,17 @@ class CuocoPrimi(Giocatore):
         super().__init__(nome, eta, esperienza)
         self.categoria = CategoriaPrimi
 
-    def prepara_portata(self) -> int:
-        pass
-
 
 class CuocoSecondi(Giocatore):
     def __init__(self, nome: str, eta: int, esperienza: dict):
         super().__init__(nome, eta, esperienza)
         self.categoria = CategoriaSecondi
 
-    def prepara_portata(self) -> int:
-        pass
-
 
 class Pasticcere(Giocatore):
     def __init__(self, nome: str, eta: int, esperienza: dict):
         super().__init__(nome, eta, esperienza)
         self.categoria = CategoriaDolci
-
-    def prepara_portata(self) -> int:
-        pass
 
 
 class Allenatore(MembroSquadra):
@@ -118,7 +134,7 @@ class Assistente(MembroSquadra):
 class Squadra:
     def __init__(self, nome: str):
         self.nome = nome
-        self.membri = []  # lista di MembroSquadra
+        self.membri = []  # lista di MembroSquadra, contiene TUTTI i ruoli
 
     def aggiungi_membro(self, membro: MembroSquadra) -> None:
         if isinstance(membro, MembroSquadra):       # verifica se l'oggetto deriva dalla quella classe
@@ -135,16 +151,84 @@ class Squadra:
         else:
             for elem in self.membri:
                 print(f"\nRuolo: {type(elem).__name__}")
-                # __name__ è un metodo che restituisce il nome della classe come stringa
+                # __name__ è un attributo che restituisce il nome della classe come stringa
                 elem.descrivi()
 
     def giocatori(self):
         # generator: yield solo istanze di Giocatore
-        pass
+        # idea: scorre tutti i giocatori della lista membri, se trova Giocatore lo restituisce
+        for elem in self.membri:
+            if isinstance(elem, Giocatore):
+                yield elem
 
     def seleziona_giocatore_per_categoria(self, categoria: type):
         """
-        categoria: classe Categoria (es. CategoriaAntipasti).
-        Deve mostrare i candidati e la loro esperienza (scala 1..5) su tutte le categorie.
+            categoria: classe Categoria (es. CategoriaAntipasti).
+
+            Il metodo individua tutti i giocatori della squadra specializzati
+            nella categoria richiesta, mostra i candidati disponibili e la loro
+            esperienza (scala 1..5) per ciascuna portata.
+
+            Tra i candidati viene selezionato un giocatore (ad esempio in modo
+            casuale) che verrà incaricato di preparare la portata.
+
+            Il metodo restituisce il giocatore scelto, così nel main sarà possibile
+            richiamare:
+
+                g1 = squadra1.seleziona_giocatore_per_categoria(cat)
+                p1 = g1.prepara_portata()
+
+            e successivamente confrontare i punteggi delle due squadre.
+
+
+        
+        print(f"\nSelezione per {categoria.label()} - {self.nome}")
+            
+        for g in self.giocatori():
+            if g.categoria == categoria:
+                print(f"\nGiocatore trovato: {g.nome}")
+                g.mostra_esperienza()
+                return g
+            
+        print("Nessun giocatore trovato per questa categoria.")
+        return None"""
+        
+        candidati = []
+
+        for g in self.giocatori():
+            if g.categoria == categoria:
+                candidati.append(g)
+
+        if len(candidati) == 0:
+            print("Nessun giocatore trovato per questa categoria.")
+            return None
+
+        print("\nCandidati disponibili:")
+        for g in candidati:
+            print(f"- {g.nome}")
+            g.mostra_esperienza()
+
+        scelto = random.choice(candidati)
+        print(f"\nScelto per la portata: {scelto.nome}")
+
+        return scelto
+    
+    def bonus_staff_round(self) -> int:
         """
-        pass
+        bonus casuale per la singola portata (round).
+        - allenatore: può dare +1 con probabilità 50%
+        - ogni Assistente: può dare +1 con probabilità 30%
+        """
+        bonus = 0
+
+        for m in self.membri:
+            if isinstance(m, Allenatore):
+                if random.random() < 0.5:
+                    bonus += 1
+            elif isinstance(m, Assistente):
+                if random.random() < 0.3:
+                    bonus += 1
+
+        return bonus
+
+
