6ef39cc18eea90ffbccbcfb85f0855137ecc358f
Ilaria Cuccaro <89212726+giushh@users.noreply.github.com>
2026-02-20 10:23:06 +0100

    mattina 20/02

--- Corso_Python/19_02_2026/pomeriggio/Eseercizio_pokemon.py
+++
@@ -1,8 +0,0 @@
-"""
-Create un programma python utilizzando le api
-https://pokeapi.co/api/v2/pokemon/ {numero} che simula un
-pokedex, quando troverete un pokemon in maniera randomica
-verificherà se è presente nel vostro pokedex (pokedex.json), in caso non fosse presente vi permetterà di catturarlo salvando le caratteristiche.
-(Sul sistema API sono presenti poco più di 1000 pokemon)
-"""
-
